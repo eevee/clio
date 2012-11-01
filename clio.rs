33b959894e2d63c53b50ca586084d6b8b46ae99e
Eevee <git@veekun.com>
2012-10-31 17:17:20 -0700

    Colors.

--- clio.rs
+++ clio.rs
@@ -6,9 +6,9 @@ use rand::task_rng;
 use amulet::ll;
 
 fn main() {
-    let map = generate_map();
-
     let window = amulet::ll::init_screen();
+
+    let map = generate_map();
 
     loop {
         draw_map(window, map);
@@ -28,11 +28,11 @@ fn draw_map(window: &amulet::ll::Window, map: @Map) {
         for uint::range(0, map.grid[x].len()) |y| {
             let tile = map.grid[x][y];
             window.mv(y, x);
-            let disp = match tile.creature {
-                Some(creature) => creature.proto.display,
-                None => tile.architecture.proto.display,
+            let proto = match tile.creature {
+                Some(creature) => creature.proto,
+                None => tile.architecture.proto,
             };
-            window.print(fmt!("%c", disp));
+            window.attrprint(fmt!("%c", proto.display), proto.style);
         }
     }
 
@@ -66,6 +66,12 @@ struct Map {
     // TODO explicit size
 }
 fn generate_map() -> @Map {
+    // TODO oh fuck, these can't be constants if they have generated Styles in them.  WHOOPS
+    let SOLID_ROCK: @Prototype = @Prototype{ display: ' ', style: ll::Style(), passable: false };
+    let WALL: @Prototype = @Prototype{ display: '▒', style: ll::Style().fg(7), passable: false };
+    let FLOOR: @Prototype = @Prototype{ display: '·', style: ll::Style().fg(8).bg(0), passable: true };
+    let PLAYER: @Prototype = @Prototype{ display: '☻', style: ll::Style().fg(0), passable: false };
+
     let width = 80;
     let height = 24;
 
@@ -79,15 +85,15 @@ fn generate_map() -> @Map {
             let proto =
                 if x < room_x || y < room_y || x >= room_x + room_width  || y >= room_y + room_height {
                     // Outside the room
-                    &SOLID_ROCK
+                    SOLID_ROCK
                 }
                 else if x == room_x || y == room_y || x == room_x + room_width - 1 || y == room_y + room_height - 1 {
                     // Edge of the room
-                    &WALL
+                    WALL
                 }
                 else {
                     // Inside of the room
-                    &FLOOR
+                    FLOOR
                 }
             ;
             @Tile{ architecture: @Entity{ proto: proto, position: (x, y) }, creature: None }
@@ -96,7 +102,7 @@ fn generate_map() -> @Map {
 
     let player_x = task_rng().gen_uint_range(room_x + 1, room_x + room_width - 1);
     let player_y = task_rng().gen_uint_range(room_y + 1, room_y + room_height - 1);
-    let player = @Entity{ proto: &PLAYER, position: (player_x, player_y) };
+    let player = @Entity{ proto: PLAYER, position: (player_x, player_y) };
     grid[player_x][player_y].creature = Some(player);
 
     return @Map{ grid: grid, player: player };
@@ -114,7 +120,7 @@ struct Tile {
 }
 
 struct Entity {
-    proto: &Prototype,
+    proto: @Prototype,
     mut position: (uint, uint),
 }
 impl Entity {
@@ -127,12 +133,8 @@ impl Entity {
 
 struct Prototype {
     display: char,
+    style: ll::Style,
     passable: bool,
 }
 
-
-const SOLID_ROCK: Prototype = Prototype{ display: ' ', passable: false };
-const WALL: Prototype = Prototype{ display: '▒', passable: false };
-const FLOOR: Prototype = Prototype{ display: ' ', passable: true };
-const PLAYER: Prototype = Prototype{ display: '☻', passable: false };
 
